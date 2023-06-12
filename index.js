8fd1c84b037e80a2d7be3dfb59337e3e6024b045
Mike Korostelev <korostelevm@gmail.com>
2023-06-12 08:53:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const sleep = ms =>
   new Promise(resolve => setTimeout(resolve, ms ));
 
 console.log('sleeping')
-sleep(2000).then(()=>{
+sleep(4000).then(()=>{
    console.log('woke up')
 let server = app.listen(3000)
 
